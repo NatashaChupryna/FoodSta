4e91e5f47b4d43ea91ce184ea253f03843002f17
Natasha <natkamonte1992@gmail.com>
2022-08-31 19:56:26 +0300

    додала ховер на стрілки слайдера

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 // Modal
 
+
 (() => {
   const refs = {
-    headerBtn: document.querySelector('[data-header-modal-open]'),
-    offeringsBtn: document.querySelector('[data-offerings-modal-open]'),
-    heroModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
     body: document.querySelector('[data-page]'),
   };
-  refs.offeringsBtn.addEventListener('click', toggleModal);
-  refs.headerBtn.addEventListener('click', toggleModal);
-  refs.heroModalBtn.addEventListener('click', toggleModal)
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
   }
-})(); 
+})();
+
 
 // Slider
 const swiper = new Swiper('.swiper', {
